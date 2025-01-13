3fdd113628a0078335be67ab3abbef6adabbd012
Adrian Winterstein <adrian@winterstein.biz>
2025-01-13 16:35:11 +0100

    Removes links from the header menu.

--- website/client/src/components/header/menu.vue
+++ website/client/src/components/header/menu.vue
@@ -315,25 +315,6 @@
               >
                 {{ $t('overview') }}
               </router-link>
-              <a
-                class="topbar-dropdown-item dropdown-item"
-                target="_blank"
-                @click.prevent="openBugReportModal()"
-              >
-                {{ $t('reportBug') }}
-              </a>
-              <a
-                class="topbar-dropdown-item dropdown-item"
-                target="_blank"
-                @click.prevent="openBugReportModal(true)"
-              >
-                {{ $t('askQuestion') }}
-              </a>
-              <a
-                class="topbar-dropdown-item dropdown-item"
-                href="https://docs.google.com/forms/d/e/1FAIpQLScPhrwq_7P1C6PTrI3lbvTsvqGyTNnGzp1ugi1Ml0PFee_p5g/viewform?usp=sf_link"
-                target="_blank"
-              >{{ $t('requestFeature') }}</a>
             </div>
           </li>
         </b-navbar-nav>
