ea17b2e9c73b95511716ed8833bba55f32221893
Phillip Thelen <phillip@habitica.com>
2026-01-21 21:34:25 +0100

    Rework how strings are localized (#15589)
    * replace lodash template usage with micromustache
    
    * remove function brackets from translations
    
    * add newline
    
    * remove old test
    
    * split core translations from content translations
    
    * fix directory not existing
    
    * fix lint

--- gulp/gulp-cache.js
+++ gulp/gulp-cache.js
@@ -33,26 +33,37 @@ gulp.task('cache:content', done => {
   }
 });
 
+function safeMkdir (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err;
+  }
+}
+
 gulp.task('cache:i18n', done => {
   // Requiring at runtime because these files access `common`
   // code which in production works only if transpiled so after
   // gulp build:babel:common has run
-  const { BROWSER_SCRIPT_CACHE_PATH, geti18nBrowserScript } = require('../website/server/libs/i18n'); // eslint-disable-line global-require
+  const { BROWSER_SCRIPT_CACHE_PATH, geti18nCoreBrowserScript, geti18nContentBrowserScript } = require('../website/server/libs/i18n'); // eslint-disable-line global-require
   const { langCodes } = require('../website/server/libs/i18n'); // eslint-disable-line global-require
 
   try {
-    // create the cache folder (if it doesn't exist)
-    try {
-      fs.mkdirSync(BROWSER_SCRIPT_CACHE_PATH);
-    } catch (err) {
-      if (err.code !== 'EEXIST') throw err;
-    }
+    // create the cache folders (if they doesn't exist)
+    safeMkdir(BROWSER_SCRIPT_CACHE_PATH);
+    safeMkdir(`${BROWSER_SCRIPT_CACHE_PATH}core/`);
+    safeMkdir(`${BROWSER_SCRIPT_CACHE_PATH}content/`);
 
     // create and save the i18n browser script for each language
     langCodes.forEach(languageCode => {
       fs.writeFileSync(
-        `${BROWSER_SCRIPT_CACHE_PATH}${languageCode}.js`,
-        geti18nBrowserScript(languageCode),
+        `${BROWSER_SCRIPT_CACHE_PATH}core/${languageCode}.js`,
+        geti18nCoreBrowserScript(languageCode),
+        'utf8',
+      );
+      fs.writeFileSync(
+        `${BROWSER_SCRIPT_CACHE_PATH}content/${languageCode}.js`,
+        geti18nContentBrowserScript(languageCode),
         'utf8',
       );
     });
